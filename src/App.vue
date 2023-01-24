c7b32b8f5ca604752a276faac4b31e914cd873c4
giadavolpin <volpingiada@gmail.com>
2023-01-24 16:53:06 +0100

    scaffolding vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+	export default {
+	name:'App',
+	data(){
+		return {
+			title:'hello'    
+		}
+	}};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
