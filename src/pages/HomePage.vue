63cce1d3570ee6d1314b1595941fefd1d2844291
giadavolpin <volpingiada@gmail.com>
2023-01-27 17:42:04 +0100

    home page modifiche

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,42 +1,32 @@
 <template>
-<div class="header">
-  <div> <a href="" id="logo"><img src="src\assets\img\logo.gif" alt=""></a> </div>  
-  <div id="navigation"></div> 
-</div>
+
 <div class="body">
-  <div class="featured"> <a href="#"><img src="" alt=""></a>
-    <div class="gallery"> <a href="#"><img src="src\assets\img\aurora.jpg" alt=""></a>
-      <ul class="first">
-        <li><a href="#"><img src="src\assets\img\summer-collection.jpg" alt=""></a></li>
-        <li><a href="#"><img src="src\assets\img\beach-bride.jpg" alt=""></a></li>
-        <li><a href="#"><img src="src\assets\img\motherhood-apparel.jpg" alt=""></a></li>
-      </ul>
-      <ul>
-        <li><a href="#"><img src="src\assets\img\fashionable.jpg" alt=""></a></li>
-        <li><a href="#"><img src="src\assets\img\accessories.jpg" alt=""></a></li>
-        <li><a href="#"><img src="src\assets\img\skin-perfect.jpg" alt=""></a></li>
+  <div class=""> 
+    <div class="gallery pt-2"> </div>
+     <div class="d-flex" > 
+      <ul class="d-flex justify-content-start flex-wrap justify-content-between p-2 m-3">
+        <li class="m-3"><a href="#"><img src="../assets/img/summer-collection.jpg" alt=""></a></li>
+        <li class="m-3"><a href="#"><img src="../assets/img/beach-bride.jpg" alt=""></a></li>
+        <li class="m-3"><a href="#"><img src="../assets/img/motherhood-apparel.jpg" alt=""></a></li>
+        <li class="m-3"><a href="#"><img src="../assets/img/fashionable.jpg" alt=""></a></li>
+        <li class="m-3"><a href="#"><img src="../assets/img/accessories.jpg" alt=""></a></li>
+        <li class="m-3"><a href="#"><img src="../assets/img/skin-perfect.jpg" alt=""></a></li>
       </ul>
     </div>
   </div>
-  <div class="article">
+  <div class="">
     <div class="first">
-      <h3>Please Read</h3>
+      <h2>Please Read</h2>
       <p>This website template has been designed by Free Website Templates for you, for free. You can replace all this text with your own text.</p>
       <p>You can remove any link to our website from this website template, you're free to use this website template without linking back to us.</p>
       <p>If you're having problems editing this website template, then don't hesitate to ask for help on the Forum.</p>
       <p>Before using a template from Free Website Templates, you must read all the for further information terms of Use</p>
     </div>
-    <div>
-      <h3>Sed Elementum</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum nibh eget justo dapibus eu porttitor purus hendrerit.</p>
-      <a href="#">Lorem ipsum dolor sit amet</a> <a href="#">Consectetur adipiscing elituisque</a> </div>
-    <div class="connect">
-      <h2>Follow us</h2>
-      <a href="#" id="facebook">Facebook</a> <a href="#" id="twitter">Twitter</a> <a href="#" id="comments">Comments</a> <a href="#" id="flickr">Flickr</a> </div>
-  </div>
-</div>
-<div class="footer">
-  <p>Copyright &copy; 2023 <a href="#">Boolean's secret</a> All rights reserved | Website Template By</p>
+    <div class="pb-3">
+      <h2>Sed Elementum</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum nibh eget justo dapibus eu porttitor purus hendrerit.</p> 
+    </div>
+    </div>   
 </div>
 </template>
 
@@ -49,4 +39,13 @@
   <style lang="scss" scoped>
 @use '../assets/styles/partials/style.scss' as *;
 
+ul li {
+  list-style: none ;
+}
+.gallery{
+  height: 470px;
+  background-image: url("../assets/img/aurora.jpg"); 
+  background-repeat: no-repeat;
+  background-size: 1000px 562px ;
+}
 </style>
