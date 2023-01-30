791fe88dbef21a6ea95429aa2943cb96b0be1ddc
LuisGonzalez27 <luis27coding@gmail.com>
2023-01-30 11:06:51 +0100

    change color button

--- src/pages/ProductList.vue
+++ src/pages/ProductList.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="prodotti" class="container-fluid pt-5 pb-5">
+    <section id="prodotti" class="container pt-5 pb-5">
         <section class="container">
             <div class="col-12 text-center">
                 <p class="display-5 fw-semibold">Esplora i nostri prodotti</p>
@@ -139,7 +139,7 @@ import { store } from "../store";
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(300px, 300px));
     gap: 2rem;
-    margin: 10px 5vw;
+    margin: 10px 0;
     padding: 0;
     list-style-type: none;
 }
